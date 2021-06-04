96214c943d22a0a0fbb7714a442378b30402e079
Doug <dcdenu4@gmail.com>
2021-06-04 15:39:52 -0400

    I. #14 adding modal pop up on launch

--- map-viewer/src/App.js
+++ map-viewer/src/App.js
@@ -5,12 +5,19 @@ import Col from 'react-bootstrap/Col';
 import React, { useSate } from 'react';
 
 import Map from './Map';
+import WelcomeModal from './components/WelcomeModal';
 import './App.css';
 
 
 function App() {
+  const [modalShow, setModalShow] = React.useState(true);
+
     return (
       <Container fluid>
+        <WelcomeModal
+          show={modalShow}
+          onHide={() => setModalShow(false)}
+        />
         <Row>
           <Col className="banner">Natural Capital Project Global Viewer
           </Col>
