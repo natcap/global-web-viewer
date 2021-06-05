bd4d8bb9cac845e19f26dc35bdf230d1a01e0ba6
Doug <dcdenu4@gmail.com>
2021-06-05 12:52:46 -0400

    I. #14 add localStorage to use for caching visit
    Added localStorage variable that allows us to know if the user is here
    for the first time or not. Only show pop up modal if first time.

--- map-viewer/src/App.js
+++ map-viewer/src/App.js
@@ -2,34 +2,45 @@ import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 
-import React, { useSate } from 'react';
+import React, { useSate, useEffect } from 'react';
 
 import Map from './Map';
 import WelcomeModal from './components/WelcomeModal';
+import WelcomeModalButton from './components/WelcomeModalButton';
 import './App.css';
 
 
 function App() {
-  const [modalShow, setModalShow] = React.useState(true);
+  const [modalShow, setModalShow] = React.useState(false);
 
-    return (
-      <Container fluid className="appContainer vh-100 d-flex flex-column">
-        <WelcomeModal
-          show={modalShow}
-          onHide={() => setModalShow(false)}
+  useEffect(() => {
+    let visited_status = localStorage.getItem('visited_status')
+    if(!visited_status) {
+      setModalShow(true);
+      localStorage.setItem('visited_status', 1);
+    }
+  },[])
+
+  return (
+    <Container fluid className="appContainer vh-100 d-flex flex-column">
+      <WelcomeModal
+        show={modalShow}
+        onHide={() => setModalShow(false)}
+      />
+      <Row>
+        <Col className="banner">Natural Capital Project Global Viewer
+        </Col>
+      </Row>
+      <Row className="h-100">
+        <Map />
+      </Row>
+      <Row className="footer align-items-center">
+        <WelcomeModalButton
+          onClick={() => setModalShow(true)}
         />
-        <Row>
-          <Col className="banner">Natural Capital Project Global Viewer
-          </Col>
-        </Row>
-        <Row className="h-100">
-          <Map />
-        </Row>
-        <Row>
-          <div className="footer"/>
-        </Row>
-      </Container>
-    );
+      </Row>
+    </Container>
+  );
 }
 
 export default App;
