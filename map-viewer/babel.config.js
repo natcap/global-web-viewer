2325ee2a43b160a654ff13347fb59fbc6fb1d0c4
Doug <dcdenu4@gmail.com>
2021-12-06 12:29:06 -0500

    babel config noparse, don't split line

--- map-viewer/babel.config.js
+++ map-viewer/babel.config.js
@@ -7,5 +7,6 @@ module.exports = {
     '@babel/plugin-transform-runtime',
     '@babel/plugin-styled-components',
   ],
+  noParse: /(mapbox-gl)\.js$/,
   ignore: [ './node_modules/mapbox-gl/dist/mapbox-gl.js' ]
 };
